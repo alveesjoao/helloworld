fd235985dea68741a5dc32673bbcc30f280de58a
Laboratório de Informática <labinfo@unifil.br>
2024-11-28 20:55:43 -0300

    adiciona funcao soma

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def main():
     print('Hello world')
 
 def cumprimento():
-    print("Olá ser humano pequeno!")
+    print("Ola ser humano pequeno!")
+
+def soma():
+    num1 = 1
+    num2 = 2
+
+    soma = num1 + num2
+    print(soma)
 
 if __name__ == "__main__":
     main()
     cumprimento()
+    soma()
